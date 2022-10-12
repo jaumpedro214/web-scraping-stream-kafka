9339b427a13d7a5ff2facdcdd554a55bdef6b64b
jaumpedro214 <joaopedrodasilvalima@gmail.com>
2022-10-12 16:54:57 -0300

    FIX: ReGex peso ignore price using $

--- src/extract_functions.py
+++ src/extract_functions.py
@@ -15,23 +15,30 @@ def extract_peso(text):
     # Format 1,5kg or 100g
     
     matches = re.findall(
-        r"\d+(?:,\d+){0,1}\s{0,1}kg|\d+g", 
+        r"[^$]{2}\s+(\d+(?:,\d+){0,1}\s{0,1}kg|\d+g)", 
         text.lower()
     )
     
     if len(matches) == 0:
         return None
 
-    peso = matches[-1]
+    # Store in grams
+    # the highest value found
+    peso = 0
+    for match in matches:
+        if "kg" in match:
+            match = match.replace("kg", "").replace(",", ".")
+            peso_match = float(match) * 1000
+        else:
+            match = match.replace("g", "")
+            peso_match = float(match)
+        
+        if peso_match > peso:
+            peso = peso_match
     
-    if "kg" in peso:
-        peso = peso.replace("kg", "").replace(",", ".")
-        return float(peso)*1000
-    else:
-        peso = peso.replace("g", "")
-        return float(peso)
+    return peso
     
 def extract_name(text):
-    # Format: name 100g
+    # All the text before the first number
     name = re.findall(r"\D+", text)[0]
     return name
